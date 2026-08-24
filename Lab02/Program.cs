83ca9b6c1d39a340cd7d9d5ce6ff7ec44fdd665e
Tanapat2002 <tanapat.yura@bumail.net>
2026-08-24 17:05:57 +0700

    Update Program.cs

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :16907071477
+ * Name       :Tanapat Yurawan
+ * Section    :129B
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true; 
+            
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"HP: {currentHp}) / {maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            Console.WriteLine();
+
+
         }
+
     }
 }
